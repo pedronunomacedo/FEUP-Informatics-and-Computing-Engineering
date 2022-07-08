8b2f95e7ef957caa2a84e8a94c4233906d1a75cf
Pedro Macedo <pedronunomacedo@hotmail.com>
2022-07-08 23:17:24 +0100

    Added the starting script file (generateStudy.sh)

--- generateStudy.sh
+++ generateStudy.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+read -p "Where do you want to save the University folder (paste the respective full path)? " fullPath
+
 cd ~
 
-echo "Where do you want to save the University folder (paste the respective path)? "
-while [true];
-do
-    read directory
-    if [[-d "${directory}" ]]; then break
-    echo "Invalid directory! Try again: "
-done
+#while [ ! -d "$fullPath" ] ; do
+#    read -p "Directory doesn't exit. Input another directory: " fullPath
+#done
+
+cd
+cd "$fullPath"
 
 mkdir Study
-
+cd Study
 mkdir Year1
 cd Year1
     mkdir Semester1
@@ -21,15 +22,27 @@ cd Year1
         git clone git@github.com:pedronunomacedo/AOCO-Year1Semester1.git
         git clone git@github.com:pedronunomacedo/FPRO-Year1Semester1.git
         git clone git@github.com:pedronunomacedo/MDIS-Year1Semester1.git
-        $valid = 0;
-        echo "Do you want to include my FEUP projet (Y/N)? "
-        while [$valid -eq 0];
-        do
-            read answer
-            if [answer -e Y || answer -e y]; then git clone git@github.com:pedronunomacedo/Project_FEUP-Year1Semester1.git; break
-            else if [answer -e N || answer -e n]; then break
-            else echo "Invalid option! Try again (Y/N): "
+        read -p "Do you want to include my FEUP projet (y/n)? " answer
+
+        i=1
+        while [ $i -ne 0 ] ; do
+            if [ $answer == "y" ] || [ $answer == "Y" ]
+            then
+                i=0
+                git clone git@github.com:pedronunomacedo/Project_FEUP-Year1Semester1.git
+                break
+            fi
+
+            if [ $answer == 'n' ] || [ $answer == 'N' ]
+            then
+                i=0
+                break
+            fi
+
+            i=1
+            read -p "Invalid option! Try again (Y/N): " answer
         done
+
         cd ..
     mkdir Semester2
     cd Semester2
@@ -40,7 +53,7 @@ cd Year1
         git clone git@github.com:pedronunomacedo/PROG-Year1Semester2.git
         cd ..
     cd..
-
+cd ..
 mkdir Year2
 cd Year2
     mkdir Semester1
@@ -50,14 +63,27 @@ cd Year2
         git clone git@github.com:pedronunomacedo/FISI2-Year2Semester1.git
         git clone git@github.com:pedronunomacedo/LDTS-Year2Semester1.git
         git clone git@github.com:pedronunomacedo/TC-Year2Semester1.git
-        echo "Do you want to include transferable skill 'Introduction to data analysis in Excel' (Y/N)? "
-        while [true]
-        do
-            read answer
-            if [answer -e Y || answer -e y]; then git@github.com:pedronunomacedo/IADE-Year2Semester1.git; break
-            else if [answer -e N || answer -e n]; then break
-            else echo "Invalid option! Try again (Y/N): "
+        read -p "Do you want to include transferable skill 'Introduction to data analysis in Excel' (Y/N)? " answer
+
+        i=1
+        while [ $i -ne 0 ] ; do
+            if [ $answer == "y" ] || [ $answer == "Y" ]
+            then
+                i=0
+                git clone git@github.com:pedronunomacedo/IADE-Year2Semester1.git
+                break
+            fi
+
+            if [ $answer == 'n' ] || [ $answer == 'N' ]
+            then
+                i=0
+                break
+            fi
+
+            i=1
+            read -p "Invalid option! Try again (Y/N): " answer
         done
+
         cd ..
     mkdir Semester2
     cd Semester2
@@ -69,4 +95,5 @@ cd Year2
         cd ..
     cd ..
 
+    clear
 echo "Cloning directory was successful. Good study!"
